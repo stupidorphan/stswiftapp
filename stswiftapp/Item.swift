46d1eeba6a4b2171541ebe21445cc9ad56d63352
Leah <me@leah.rocks>
2026-07-04 10:45:44 -0700

    Initial Commit

---
+++ stswiftapp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  stswiftapp
+//
+//  Created by Leah on 7/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ stswiftappTests/stswiftappTests.swift
@@ -0,0 +1,17 @@
+//
+//  stswiftappTests.swift
+//  stswiftappTests
+//
+//  Created by Leah on 7/4/26.
+//
+
+import Testing
+@testable import stswiftapp
+
+struct stswiftappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
